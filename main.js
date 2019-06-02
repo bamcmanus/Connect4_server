ac4502fae9e4d7fe227c72b2d5eafe8847fe44d5
nro2 <nro2@pdx.edu>
2019-06-01 19:34:16 -0700

    Added checking for vertical win.

--- main.js
+++ main.js
@@ -1,40 +1,84 @@
 //empty = 0
-//red = 1
-//black = 2
+//black = 1
+//red = 2
+
 const ROWS = 6;
 const COLS = 7;
+var isOver = false;
+var lastCol = 0;
 
-const boardMap = Array.from({ length:ROWS}, () => (
-    Array.from( {length:COLS}, ()=> 0)
+const boardMap = Array.from({ length: ROWS }, () => (
+    Array.from({ length: COLS }, () => 0)
 ));
-console.log(boardMap);
-var player = 'black';
+
+
+var player = 'Black';
 alert('Black starts!')
 $('td').click(function () {
     var myClass = $(this).attr("class");
-    var result = makeMove(myClass);   
-    console.log(boardMap);
+    makeMove(myClass);
+    var result = checkWinner(lastCol);
+    if (result === true) {
+        gameOver();
+    }
+
 });
 
 function makeMove(myClass) {
-    $($('.' + myClass).get().reverse()).each(function(index) {
+    $($('.' + myClass).get().reverse()).each(function (index) {
         if (!$(this).hasClass("red") && !$(this).hasClass("black")) {
             var colNum = parseInt(this.className.charAt(3), 10);
-            if (player === 'black'){
-               $(this).addClass('black');
-               player = 'red';
-               alert(player + ' player is up!' + ' index is ' + index + '. Colnum is ' + colNum)
-               boardMap[ROWS - 1 - index][colNum] = 2;  
+
+            if (player === 'Black') {
+                $(this).addClass('black');
+                player = 'Red';
+                lastCol = colNum;
+                boardMap[ROWS - 1 - index][colNum] = 1;
             }
-            else if (player === 'red'){
-               $(this).addClass('red'); 
-               player = 'black';
-               alert(player + ' player is up!' + ' index is ' + index)
-               boardMap[ROWS - 1 - index][colNum] = 1;
+            else if (player === 'Red') {
+                $(this).addClass('red');
+                player = 'Black';
+                lastCol = colNum;
+                boardMap[ROWS - 1 - index][colNum] = 2;
             }
             return false;
         }
     });
+};
 
+function checkWinner(lastCol) {
+    if (is_winner_vert(lastCol)) {
+        return true;
+    }
+    return false;
+};
 
+function is_winner_vert(lastCol) {
+    let count = 0;
+    for (let i = 0; i < ROWS; i++) {
+        if (boardMap[i][lastCol] === 1 || boardMap[i][lastCol] === 2) {
+            for (let j = i + 1; j < ROWS; j++) {
+                if (boardMap[j][lastCol] == boardMap[i][lastCol]) {
+                    count++;
+                    console.log(boardMap[j][lastCol] + ' is first and ' + boardMap[i][lastCol] + ' is second and count is ' + count)
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+    if (count >= 4) {
+        return true;
+    }
+    return false;
 }
+function gameOver() {
+    if (player === 'Black') {
+        alert('Game over, red wins!');
+    }
+    else {
+        alert('Game over, black wins!');
+    }
+};
+
